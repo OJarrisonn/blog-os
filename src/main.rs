92daa6618b970f1caaeed8223698664cdb4a3e71
OJarrisonn <j.h.m.t.v.10@gmail.com>
2023-08-03 09:10:24 -0300

    Testing & VGA Text Buffer

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,33 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(blog_os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
+use blog_os::println;
 
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+// static HELLO: &[u8] = b"Hello World";
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    println!("Hello World{}", "!!!");
+
+    #[cfg(test)]
+    test_main();
+
     loop { }
 }
 
-static HELLO: &[u8] = b"Hello World";
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
+// Panic handlers
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop { }
+}
 
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-
-    loop {
-        
-    }
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    blog_os::test_panic_handler(info)
 }
